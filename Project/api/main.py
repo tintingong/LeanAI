e5892d39560498623dd8abbbe28f999a215f97f1
baxik88 <baxwork88@gmail.com>
2025-03-18 21:49:25 -0400

    added front form

--- Project/api/main.py
+++ Project/api/main.py
@@ -3,29 +3,50 @@ from fastapi.templating import Jinja2Templates
 import pickle
 import numpy as np
 from pathlib import Path
+from os import getenv
 
 app = FastAPI()
 templates = Jinja2Templates(directory="api/templates")
 
-# Load model
-MODEL_PATH = Path("models/model.pkl")
+MODEL_PATH = Path(getenv("MODEL_PATH", "/app/models/model.pkl"))
+print("MODEL_PATH:", MODEL_PATH)
+
 if MODEL_PATH.exists():
     with open(MODEL_PATH, "rb") as f:
         model = pickle.load(f)
+        print("---------------   Model loaded")
+        print(model)
+        print('MODEL TYPE: ', type(model))
 else:
     model = None
+    print("Model not found or failed to load.")
 
 @app.get("/")
 def form_page(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
 
 @app.post("/predict/")
-def predict(request: Request, feature1: float = Form(...), feature2: float = Form(...)):
-    if not model:
+def predict(
+    request: Request,
+    abdomen: float = Form(...),
+    hip: float = Form(...),
+    weight: float = Form(...),
+    thigh: float = Form(...),
+    knee: float = Form(...),
+    biceps: float = Form(...),
+    neck: float = Form(...),
+):
+    if model is None:
         return {"error": "Model not found"}
 
-    features = np.array([[feature1, feature2]])
+    # Создаем массив с фичами
+    features = np.array([[abdomen, weight, chest, hip, thigh, knee, biceps, density]])
+
+    # Получаем предсказание
+    print("Input shape:", features.shape)
     prediction = model.predict(features)
+    print("Prediction result:", prediction)
+
     return templates.TemplateResponse("form.html", {
         "request": request,
         "prediction": prediction[0]
